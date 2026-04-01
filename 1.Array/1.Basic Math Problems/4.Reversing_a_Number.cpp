01ad0fbdba6a2a9c25cf1d57d36f275f0b01e2fc
KING <matadeenyadav202@gmail.com>
2026-04-01 06:55:23 +0530

    Create 4.Reversing_a_Number.cpp

---
+++ 1.Array/1.Basic Math Problems/4.Reversing_a_Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverseNumber(int num) {
+        // Implement logic to reverse the number
+        int ans = 0 ;
+
+        while(num!=0){
+            int digit = num % 10;
+            //reverse number creation;
+            ans = ans*10+digit;
+            //num updation to avoid infinite loop
+            num = num/10;
+        }
+        return ans;
+    }
+};
