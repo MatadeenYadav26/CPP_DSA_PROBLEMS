cc2b8c54f1a3b5485ce54fe6e767f9f24ad343b4
KING <matadeenyadav202@gmail.com>
2026-04-01 06:25:49 +0530

    Create 2.smallestDigit.cpp

---
+++ 1.Array/1.Basic Math Problems/2.smallestDigit.cpp
@@ -0,0 +1,21 @@
+#include<limits.h>
+class Solution {
+public:
+    int smallestDigit(int num) {
+        // Implement logic to find the smallest digit
+        int smallest = INT_MAX;
+        while(num!=0){
+            int digit = num % 10;
+            //check if digit is smallest or not
+            //if jo new digit mere paas aya hai,
+            //agar vo chota h ab tak kk smallest value se ,
+            //then store this new digit
+            if(digit < smallest){
+                smallest = digit;
+            }
+            // update the loop to avoid infinite loop
+            num = num /10;
+        }
+        return smallest;
+    }
+};
