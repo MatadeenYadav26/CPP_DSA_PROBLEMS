5054d60ba1d31708cb18f11c37fa0ec889951aa0
Matadeen Yadav <matadeenyadav202@gmail.com>
2026-04-08 18:41:06 +0530

    Create 11. Printing unique pairs : same_same included.cpp

---
+++ 1.Array/3.Problems on Array/11. Printing unique pairs : same_same included.cpp
@@ -0,0 +1,16 @@
+// Same pairs included but not opposite
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i = 0; i < n; i++) {
+    for(int j = i; j < n; j++) {
+        cout << "(" << arr[i] << ", " << arr[j] << ")" << endl;
+    }
+}
+
+    return 0;
+}
