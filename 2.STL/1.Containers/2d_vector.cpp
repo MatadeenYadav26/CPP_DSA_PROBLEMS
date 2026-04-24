14cb3e6a79c2744f0e75ad7dc3e4fa605f040260
Matadeen Yadav <matadeenyadav202@gmail.com>
2026-04-24 13:17:18 +0530

    Add files via upload

---
+++ 2.STL/1.Containers/2d_vector.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+
+
+    // 2d array :
+
+    vector<vector<int> > arr(5 , vector<int>(4,0));
+
+    // 2d array created w 5 rows , 4 columns , initial value of each as 0 : 
+
+    // count of rows : 
+
+    
+}
